4aff27a430d65e73d46e8337fb3cf0623083f5c7
dancingcookieman <74688350+dancingcookieman@users.noreply.github.com>
2021-06-11 13:16:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(900,900)
+    canvas.center()
+}
+function draw(){
+
+}
+function take_screenshot(){
+    save("filtered_image")
+}
